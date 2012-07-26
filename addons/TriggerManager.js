9d6d92df31ec746cdd058c3017a726fdb6a187fd
Stefano Balietti <futur.dorko@gmail.com>
2012-07-26 14:54:51 +0200

    added doc for TriggerManager (almost complete)

--- addons/TriggerManager.js
+++ addons/TriggerManager.js
@@ -5,12 +5,28 @@
  * Copyright(c) 2012 Stefano Balietti
  * MIT Licensed 
  * 
- * Manages a collection of function called sequentially
+ * Manages a collection of trigger functions to be called sequentially
  *  
- * this.return flag determines how the hooks are called:
+ * ## Note for developers
  * 
- * - 'first': returns the value from the first trigger which matches the object
- * - 'last': returns the value from the last trigger, after all have been executed
+ * Triggers are functions that operate on a common object, and each 
+ * sequentially adds further modifications to it. 
+ * 
+ * If the TriggerManager were a beauty saloon, the first trigger function
+ * would wash the hair, the second would cut the washed hair, and the third
+ * would style it. All these operations needs to be done sequentially, and
+ * the TriggerManager takes care of handling this process.
+ * 
+ * If `TriggerManager.return` is set equal to `TriggerManager.first`, 
+ * the first trigger function returning a truthy value will stop the process
+ * and the target object will be immediately returned. In these settings,
+ * if a trigger function returns `undefined`, the target is passed to the next
+ * trigger function. 
+ * 
+ * Notice: TriggerManager works as a *LIFO* queue, i.e. new trigger functions
+ * will be executed first.
+ * 
+ * ---
  * 
  */
 
@@ -23,6 +39,8 @@ exports.TriggerManager = TriggerManager;
 TriggerManager.first = 'first';
 TriggerManager.last = 'last';
 
+var triggersArray;
+
 /**
  * ## TriggerManager constructor
  * 
@@ -30,118 +48,192 @@ TriggerManager.last = 'last';
  * 
  */
 function TriggerManager (options) {
-// ## Public properties
+// ## Private properties
 	
-	this.options = options = options || {};
-	this.triggers = [];
-	this.return = TriggerManager.first; // options are first, last 
-	this.init(this.options);
-	
-	Object.defineProperty(this, 'length', {
-    	set: function(){},
-    	get: function(){
-    		return this.triggers.length;
-    	},
-    	configurable: true
+/**
+ * ### TriggerManager.triggers
+ * 
+ * Array of trigger functions 
+ * 
+ */
+	triggersArray = [];
+	Object.defineProperty(this, 'triggers', {
+		value: triggersArray,
+		enumerable: true,
 	});
+	
+// ## Public properties
+
+/**
+ * ### TriggerManager.options
+ * 
+ * Reference to current configuration
+ * 
+ */	
+	this.options = options || {};
+
+/**
+ * ### TriggerManager.return
+ * 
+ * Controls the behavior of TriggerManager.pullTriggers
+ * 
+ */	
+	this.return = TriggerManager.first; // options are first, last 
+
+
+/**
+ * ### TriggerManager.length
+ * 
+ * The number of registered trigger functions
+ * 
+ */
+	Object.defineProperty(this, 'length', {
+		set: function(){},
+		get: function(){
+			return triggersArray.length;
+		},
+		configurable: true
+	});
+	
+	this.init();
 };
 
 // ## TriggerManager methods
 
 /**
- * ### TriggerManager.
+ * ### TriggerManager.init
  * 
+ * Configures the TriggerManager instance
+ * 
+ * Takes the configuration as an input parameter or 
+ * recycles the settings in `this.options`.
+ * 
+ * The configuration object is of the type
+ * 
+ * 	var options = {
+ * 		return: 'first', // or 'last'
+ * 		triggers: [ myFunc,
+ * 					myFunc2 
+ * 		],
+ * 	} 
+ * 	 
+ * @param {object} options Optional. Configuration object
  * 
  */
-TriggerManager.prototype.init = function(options) {
+TriggerManager.prototype.init = function (options) {
 	this.options = options || this.options;
 	if (this.options.return === TriggerManager.first || this.options.return === TriggerManager.last) {
-		this.return = this.options.return || this.return;
+		this.return = this.options.return;
 	}
 	this.resetTriggers();
 };
 
 /**
- * ### TriggerManager.
+ * ### TriggerManager.initTriggers
  * 
+ * Adds a collection of trigger functions to the trigger array
  * 
+ * @param {function|array} triggers An array of trigger functions or a single function 
  */
-TriggerManager.prototype.initTriggers = function(triggers) {
-	if (triggers) {
-		if (!(triggers instanceof Array)) {
-			triggers = [triggers];
-		}
-		for (var i=0; i< triggers.length; i++) {
-			this.triggers.push(triggers[i]);
-		}
-	} 
+TriggerManager.prototype.initTriggers = function (triggers) {
+	if (!triggers) return;
+	
+	if (!(triggers instanceof Array)) {
+		triggers = [triggers];
+	}
+	for (var i=0; i< triggers.length; i++) {
+		triggersArray.push(triggers[i]);
+	}
   };
 	
 /**
- * ### TriggerManager.
+ * ### TriggerManager.resetTriggers
  *   
- * Delete existing render functions and add the default
- * ones, if any.
+ * Resets the trigger array to initial configuration
+ *   
+ * Delete existing trigger functions and re-add the ones
+ * contained in `TriggerManager.options.triggers`
+ * 
  */
 TriggerManager.prototype.resetTriggers = function () {
-	this.triggers = [];
+	triggersArray = [];
 	this.initTriggers(this.options.triggers);
 };
 
 /**
- * ### TriggerManager.
+ * ### TriggerManager.clear
  * 
+ * Clears the trigger array
  * 
+ * Requires a boolean parameter to be passed for confirmation
+ * 
+ * @param {boolean} clear TRUE, to confirm clearing
+ * @return {boolean} TRUE, if clearing was successful
  */
 TriggerManager.prototype.clear = function (clear) {
 	if (!clear) {
 		node.log('Do you really want to clear the current TriggerManager obj? Please use clear(true)', 'WARN');
 		return false;
 	}
-	this.triggers = [];
+	triggersArray = [];
 	return clear;
 };
 	
 /**
- * ### TriggerManager.
+ * ### TriggerManager.addTrigger
  * 
+ * Pushes a trigger into the trigger array
  * 
+ * @param {function} trigger The function to add
+ * @param {number} pos Optional. The index of the trigger in the array
+ * @return {boolean} TRUE, if insertion is successful
  */	  
 TriggerManager.prototype.addTrigger = function (trigger, pos) {
-	if (!trigger) return;
+	if (!trigger) return false;
+	if (!('function' === typeof trigger)) return false;
 	if (!pos) {
-		this.triggers.push(trigger);
+		triggersArray.push(trigger);
 	}
 	else {
-		this.triggers.splice(pos, 0, trigger);
+		triggersArray.splice(pos, 0, trigger);
 	}
 	return true;
 };
 	  
 /**
- * ### TriggerManager.
+ * ### TriggerManager.removeTrigger
  * 
  * 
  */	  
 TriggerManager.prototype.removeTrigger = function (trigger) {
-	for (var i=0; i< this.triggers.length; i++) {
-		if (this.triggers[i] == trigger) {
-			return this.triggers.splice(i,1);
+	for (var i=0; i< triggersArray.length; i++) {
+		if (triggersArray[i] == trigger) {
+			return triggersArray.splice(i,1);
 		}
 	}  
 	return false;
 };
 
 /**
- * ### TriggerManager.
+ * ### TriggerManager.pullTriggers
  * 
+ * Fires the collection of trigger functions on the target object
  * 
+ * Triggers are fired according to a LIFO queue, i.e. new trigger
+ * functions are fired first.
+ * 
+ * Depending on the value of `TriggerManager.return`, the target o
+ * 
+ * 	- 'first': the first trigger which matches the object
+ * 	- 'last': the last trigger, after all have been executed
+ * 
+ * If `TriggerManager.first` 
  */	
 TriggerManager.prototype.pullTriggers = function (o) {
 	if (!o) return;
-	// New criteria are fired first
-	for (var i = this.triggers.length; i > 0; i--) {
-		var out = this.triggers[(i-1)].call(this, o);
+	
+	for (var i = triggersArray.length; i > 0; i--) {
+		var out = triggersArray[(i-1)].call(this, o);
 		if (out) {
 			if (this.return === TriggerManager.first) {
 				return out;
@@ -153,12 +245,16 @@ TriggerManager.prototype.pullTriggers = function (o) {
 };
 
 /**
- * ### TriggerManager.
+ * ### TriggerManager.size
  * 
+ * Returns the number of registered trigger functions
  * 
+ * Use TriggerManager.length instead 
+ * 
+ * @deprecated
  */
 TriggerManager.prototype.size = function () {
-	return this.triggers.length;
+	return triggersArray.length;
 };
 	
 
