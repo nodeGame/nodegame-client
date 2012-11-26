43d67b574674798f5ed84f65794bd90adce15eae
Stefano Balietti <futur.dorko@gmail.com>
2012-11-26 23:34:50 +0100

    added compatibility to JSUS build

--- bin/build.js
+++ bin/build.js
@@ -59,6 +59,7 @@ var ng_addons = [
 var JSUSdir = J.resolveModuleDir('JSUS');
 
 var ng_jsus = [
+	JSUSdir + "lib/compatibility.js",
 	JSUSdir + "jsus.js",
 	JSUSdir + "lib/array.js",
 	JSUSdir + "lib/dom.js",
