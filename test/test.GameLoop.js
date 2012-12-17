7401ae3b262ab969a59522c04c6a01ed3b509756
Stefano Balietti <futur.dorko@gmail.com>
2012-12-17 17:05:25 +0100

    test for travis

--- test/test.GameLoop.js
+++ test/test.GameLoop.js
@@ -84,9 +84,11 @@ describe('GameLoop', function() {
 	});
 	
 	describe('#next()', function() {
-//		it('should return 1.1.1', function() {
-//			GameState.compare(gameLoop.next(),'1.1.1').should.be.equal(0); 
-//		});
+		it('should return 1.1.1', function() {
+			console.log(node.game.state)
+			console.log(gameLoop.next())
+			GameState.compare(gameLoop.next(),'1.1.1').should.be.equal(0); 
+		});
 		it('should return 2.1.1', function() {
 			GameState.compare(gameLoop.next('1.1.1'),'2.1.1').should.be.equal(0); 
 		});
