5ca29eb4e65ca16aabf1f0ceb08b6e729061afa2
Stefano Balietti <futur.dorko@gmail.com>
2013-03-23 19:34:22 +0100

    tests working

--- test/test.Stager.js
+++ test/test.Stager.js
@@ -91,106 +91,93 @@ describe('Stager', function() {
 		});
 	});
 	
-//	describe('#next()', function() {
+	describe('#next()', function() {
 //		it('should return 1.1.1', function() {
-////			console.log(node.game.state)
-////			console.log(stager.next())
 //			GameState.compare(stager.next(),'1.1.1').should.be.equal(0); 
 //		});
-//		it('should return 2.1.1', function() {
-//			GameState.compare(stager.next('1.1.1'),'2.1.1').should.be.equal(0); 
-//		});
-//		it('should return 3.1.1', function() {
-//			GameState.compare(stager.next('2.1.1'),'3.1.1').should.be.equal(0); 
-//		});
-//		it('should return 3.2.1', function() {
-//			GameState.compare(stager.next('3.1.1'),'3.2.1').should.be.equal(0); 
-//		});
-//		it('should return 3.3.1', function() {
-//			GameState.compare(stager.next('3.2.1'),'3.3.1').should.be.equal(0); 
-//		});
-//		it('should return 3.1.2', function() {
-//			GameState.compare(stager.next('3.3.1'),'3.1.2').should.be.equal(0); 
-//		});
-//		
-//		it('should return false when reached the end of the stages', function() {
-//			stager.next('5.1.1').should.be.false; 
-//		});
-//	});
+		it('should return 2.1.1', function() {
+			GameState.compare(stager.next('1.1.1'),'2.1.1').should.be.equal(0); 
+		});
+		it('should return 3.1.1', function() {
+			GameState.compare(stager.next('2.1.1'),'3.1.1').should.be.equal(0); 
+		});
+		it('should return 3.2.1', function() {
+			GameState.compare(stager.next('3.1.1'),'3.2.1').should.be.equal(0); 
+		});
+		it('should return 3.3.1', function() {
+			GameState.compare(stager.next('3.2.1'),'3.3.1').should.be.equal(0); 
+		});
+		it('should return 3.1.2', function() {
+			GameState.compare(stager.next('3.3.1'),'3.1.2').should.be.equal(0); 
+		});
+		
+		it('should return false when reached the end of the stages', function() {
+			stager.next('5.1.1').should.be.false; 
+		});
+	});
 //	
-//	describe('#previous()', function() {
-//		it('should return 1.1.1', function() {
-//			GameState.compare(stager.previous('2.1.1'),'1.1.1').should.be.equal(0); 
-//		});
-//		it('should return 2.1.1', function() {
-//			GameState.compare(stager.previous('3.1.1'),'2.1.1').should.be.equal(0); 
-//		});
-//		it('should return 3.1.1', function() {
-//			GameState.compare(stager.previous('3.2.1'),'3.1.1').should.be.equal(0); 
-//		});
-//		it('should return 3.2.1', function() {
-//			GameState.compare(stager.previous('3.3.1'),'3.2.1').should.be.equal(0); 
-//		});
-//		it('should return 3.3.1', function() {
-//			GameState.compare(stager.previous('3.1.2'),'3.3.1').should.be.equal(0); 
-//		});
-//		
-//		it('should return false at the beginning of the stages', function() {
-//			stager.previous('1.1.1').should.be.false; 
-//		});
-//	});
-//	
-//	describe('#jumpTo() forward', function() {
-//		it('should return 2.1.1', function() {
-//			GameState.compare(stager.jumpTo('1.1.1', 1), '2.1.1').should.be.equal(0); 
-//		});
-//		it('should return 3.1.1', function() {
-//			GameState.compare(stager.jumpTo('3.1.1', 1), '3.2.1').should.be.equal(0); 
-//		});
-//		it('should return 3.2.1', function() {
-//			GameState.compare(stager.jumpTo('3.3.1', 1), '3.1.2').should.be.equal(0); 
-//		});
-//		it('should return 3.3.1', function() {
-//			GameState.compare(stager.jumpTo('3.1.1', 2), '3.3.1').should.be.equal(0); 
-//		});
-//		it('should return 3.3.1', function() {
-//			GameState.compare(stager.jumpTo('3.1.1', 5), '3.3.2').should.be.equal(0); 
-//		});
-//		it('should return false at the beginning of the stages', function() {
-//			stager.jumpTo('5.1.1',1).should.be.false; 
-//		});
-//	});
-//	
-//	describe('#jumpTo() backward', function() {
-//		it('should return 1.1.1 (jump -1)', function() {
-//			GameState.compare(stager.jumpTo('2.1.1', -1), '1.1.1').should.be.equal(0); 
-//		});
-//		it('should return 3.1.1 (jump -1)', function() {
-//			GameState.compare(stager.jumpTo('3.2.1', -1), '3.1.1').should.be.equal(0); 
-//		});
-//		it('should return 3.3.1 (jump -1)', function() {
-//			GameState.compare(stager.jumpTo('3.1.2', -1), '3.3.1').should.be.equal(0); 
-//		});
-//		it('should return 3.1.1 (jump -2)', function() {
-//			GameState.compare(stager.jumpTo('3.3.1', -2), '3.1.1').should.be.equal(0); 
-//		});
-//		it('should return 3.1.1 (jump -5)', function() {
-//			GameState.compare(stager.jumpTo('3.3.2', -5), '3.1.1').should.be.equal(0); 
-//		});
-//		it('should return false at the beginning of the stages', function() {
-//			stager.jumpTo('1.1.1',-1).should.be.false; 
-//		});
-//		
-//	});
+	describe('#previous()', function() {
+		it('should return 1.1.1', function() {
+			GameState.compare(stager.previous('2.1.1'),'1.1.1').should.be.equal(0); 
+		});
+		it('should return 2.1.1', function() {
+			GameState.compare(stager.previous('3.1.1'),'2.1.1').should.be.equal(0); 
+		});
+		it('should return 3.1.1', function() {
+			GameState.compare(stager.previous('3.2.1'),'3.1.1').should.be.equal(0); 
+		});
+		it('should return 3.2.1', function() {
+			GameState.compare(stager.previous('3.3.1'),'3.2.1').should.be.equal(0); 
+		});
+		it('should return 3.3.1', function() {
+			GameState.compare(stager.previous('3.1.2'),'3.3.1').should.be.equal(0); 
+		});
+		
+		it('should return false at the beginning of the stages', function() {
+			stager.previous('1.1.1').should.be.false; 
+		});
+	});
 	
-//	describe('cleanup operations for testing on travis-ci', function() {
-//		before(function(){
-//			console.log('--------')
-//			console.log(node.game.state);
-////			node.game.state = new GameState();
-//		});
-//		it('testing for travis-ci', function() {
-//			console.log(node.game.state)
-//		})
-//	});
+	describe('#jumpTo() forward', function() {
+		it('should return 2.1.1', function() {
+			GameState.compare(stager.jumpTo('1.1.1', 1), '2.1.1').should.be.equal(0); 
+		});
+		it('should return 3.1.1', function() {
+			GameState.compare(stager.jumpTo('3.1.1', 1), '3.2.1').should.be.equal(0); 
+		});
+		it('should return 3.2.1', function() {
+			GameState.compare(stager.jumpTo('3.3.1', 1), '3.1.2').should.be.equal(0); 
+		});
+		it('should return 3.3.1', function() {
+			GameState.compare(stager.jumpTo('3.1.1', 2), '3.3.1').should.be.equal(0); 
+		});
+		it('should return 3.3.1', function() {
+			GameState.compare(stager.jumpTo('3.1.1', 5), '3.3.2').should.be.equal(0); 
+		});
+		it('should return false at the beginning of the stages', function() {
+			stager.jumpTo('5.1.1',1).should.be.false; 
+		});
+	});
+	
+	describe('#jumpTo() backward', function() {
+		it('should return 1.1.1 (jump -1)', function() {
+			GameState.compare(stager.jumpTo('2.1.1', -1), '1.1.1').should.be.equal(0); 
+		});
+		it('should return 3.1.1 (jump -1)', function() {
+			GameState.compare(stager.jumpTo('3.2.1', -1), '3.1.1').should.be.equal(0); 
+		});
+		it('should return 3.3.1 (jump -1)', function() {
+			GameState.compare(stager.jumpTo('3.1.2', -1), '3.3.1').should.be.equal(0); 
+		});
+		it('should return 3.1.1 (jump -2)', function() {
+			GameState.compare(stager.jumpTo('3.3.1', -2), '3.1.1').should.be.equal(0); 
+		});
+		it('should return 3.1.1 (jump -5)', function() {
+			GameState.compare(stager.jumpTo('3.3.2', -5), '3.1.1').should.be.equal(0); 
+		});
+		it('should return false at the beginning of the stages', function() {
+			stager.jumpTo('1.1.1',-1).should.be.false; 
+		});
+		
+	});
 });
