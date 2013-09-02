db415dd5eb47818870723b36f2c8a2515f9d87e5
Stefano Balietti <futur.dorko@gmail.com>
2013-09-02 09:49:36 +0200

    Fixed PlayerList bug when initializing a new object with non-empty db parameter

--- test/test.PlayerList.js
+++ test/test.PlayerList.js
@@ -47,12 +47,11 @@ player4 = new Player ({
 });
 
 
-var pl = new PlayerList();
+// If updated, updates the tests.
+var plDB = [player, player2, player3, player4];
 
-
-//console.log(pl.get.toString());
-//
-//console.log(util.inspect(pl.__proto__));
+// The playerList object.
+var pl;
 
 // Check if pl2 == pl1
 function samePlayer(pl1, pl2) {
@@ -63,65 +62,72 @@ function samePlayer(pl1, pl2) {
 
 describe('PlayerList', function() {
     
-    describe('#add()', function() {
-	before(function(){
-	    pl.add(player);
+    describe('#constructor', function() {
+	it('with no paramters: player list length 0', function() {     
+            pl = new PlayerList();
+	    pl.size().should.equal(0);
 	});
-	it('should result in a player list of length 1', function() {
-	    pl.length.should.equal(1);
+        it('with a list of players: player list length 4', function() {     
+            pl = new PlayerList(null, plDB);
+	    pl.size().should.equal(plDB.length);
 	});
-	
+	it('with a list of players: should have for elements in the ID index', function() {
+            pl = new PlayerList(null, plDB);
+            pl.rebuildIndexes();
+	    pl.id.size().should.equal(plDB.length);
+	});
     });
+
     
-    //	describe('#get()', function() {
-    //		before(function(){
-    //			test_player = pl.get(player.id);
-    //		});
-    //		it('should return the player we have inserted before', function() {
-    //			samePlayer(player, test_player);
-    //		});
-    //		
-    //	});
-    //	
-    //	describe('#pop()', function() {
-    //		before(function() {
-    //			test_player = pl.pop(player.id);
-    //		});
-    //		
-    //		it('should return a player object', function() {
-    //			test_player.should.exists;
-    //		});
-    //		
-    //		it('should return the player we have inserted before', function() {
-    //			samePlayer(player, test_player);
-    //		});
-    //		
-    //		it('should remove the player we have inserted before', function() {
-    //			pl.length.should.equal(0);
-    //		});
-    //	});
-    //	
-    //	describe('#getRandom()', function() {
-    //		before(function(){
-    //			pl.add(player);
-    //			pl.add(player2);
-    //			pl.add(player3);
-    //			pl.add(player4);
-    //		});
-    //		
-    //		it('should return one random player', function() {
-    //			var r = pl.getRandom();
-    //			r.name.should.exist;
-    //			r.should.be.a('object');
-    //			[player,player2,player3,player4].should.include(r);
-    //			
-    //		});
-    //		
-    //		it('should return two random players', function() {
-    //			var set = pl.getRandom(2);
-    //			set.length.should.equal(2);
-    //		});
-    //	});
+//    describe('#get()', function() {
+//	before(function(){
+//	    test_player = pl.get(player.id);
+//	});
+//	it('should return the player we have inserted before', function() {
+//	    samePlayer(player, test_player);
+//	});
+//	
+//    });
+//    
+//    describe('#pop()', function() {
+//	before(function() {
+//	    test_player = pl.pop(player.id);
+//	});
+//	
+//	it('should return a player object', function() {
+//	    test_player.should.exists;
+//	});
+//	
+//	it('should return the player we have inserted before', function() {
+//	    samePlayer(player, test_player);
+//	});
+//	
+//	it('should remove the player we have inserted before', function() {
+//	    pl.length.should.equal(0);
+//	});
+//    });
+//    
+//    describe('#getRandom()', function() {
+//	before(function(){
+//	    pl.add(player);
+//	    pl.add(player2);
+//	    pl.add(player3);
+//	    pl.add(player4);
+//	});
+//	
+//	it('should return one random player', function() {
+//	    var r = pl.getRandom();
+//	    r.name.should.exist;
+//	    r.should.be.a('object');
+//	    [player,player2,player3,player4].should.include(r);
+//	    
+//	});
+//	
+//	it('should return two random players', function() {
+//	    var set = pl.getRandom(2);
+//	    set.length.should.equal(2);
+//	});
+//    });
     
     
 });
