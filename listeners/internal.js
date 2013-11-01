46f5992b261935b72a4f679f9138af42d878c108
Stefano Balietti <futur.dorko@gmail.com>
2013-11-01 12:13:01 +0100

    syncStepping option

--- listeners/internal.js
+++ listeners/internal.js
@@ -4,7 +4,7 @@
 // but they are usually responding to internal nodeGame events,
 // such as progressing in the loading chain, or finishing a game stage.
 
-(function (exports, parent) {
+(function(exports, parent) {
 
     "use strict";
 
@@ -27,13 +27,14 @@
     OUT = constants.OUT;
 
     var gcommands = constants.gamecommands;
+    var CMD = 'NODEGAME_GAMECOMMAND_';
 
     /**
      * ## NodeGameClient.addDefaultInternalListeners
      *
      * Adds a battery of event listeners for internal events
      *
-     * If executed once, it requires a force flag to re-add the listeners
+     * If executed once, it requires a force flag to re-add the listeners.
      *
      * @param {boolean} TRUE, to force re-adding the listeners
      * @return {boolean} TRUE on success
@@ -41,26 +42,28 @@
     NGC.prototype.addDefaultInternalListeners = function(force) {
         var node = this;
         if (this.internalAdded && !force) {
-            this.err('Default internal listeners already added once. Use the force flag to re-add.');
+            this.err('Default internal listeners already added once. ' +
+                     'Use the force flag to re-add.');
             return false;
         }
 
         /**
          * ## DONE
          *
-         * Updates and publishes that the client has successfully terminated a stage
+         * Registers the stageLevel _DONE_ and eventually steps forward.
          *
-         * If a DONE handler is defined in the game-plot, it will executes it before
-         * continuing with further operations. In case it returns FALSE, the update
+         * If a DONE handler is defined in the game-plot, it will execute it. 
+         * In case it returns FALSE, the update
          * process is stopped.
          *
          * @emit BEFORE_DONE
-         *
          */
         this.events.ng.on('DONE', function() {
             // Execute done handler before updating stage.
-            var ok = true,
-                done = node.game.getCurrentStep().done;
+            var ok, done;
+            ok = true;
+            done = node.game.plot.getProperty(node.game.getCurrentGameStage(),
+                                              'done');
 
             if (done) ok = done.apply(node.game, arguments);
             if (!ok) return;
@@ -75,7 +78,7 @@
                 node.game.step();
             }
         });
-        
+
         /**
          * ## STEP_CALLBACK_EXECUTED
          *
@@ -86,7 +89,7 @@
                 node.emit('LOADED');
             }
         });
-        
+
         /**
          * ## WINDOW_LOADED
          *
@@ -136,7 +139,7 @@
          * ## NODEGAME_GAMECOMMAND: start
          *
          */
-        this.events.ng.on('NODEGAME_GAMECOMMAND_' + gcommands.start, function(options) {
+        this.events.ng.on(CMD + gcommands.start, function(options) {
             node.emit('BEFORE_GAMECOMMAND', gcommands.start, options);
 
             if (node.game.getCurrentStep() &&
@@ -150,12 +153,12 @@
         });
 
         /**
-         * ## NODEGAME_GAMECOMMAND: pause
+         * ## NODEGAME_GAMECMD: pause
          *
          */
-        this.events.ng.on('NODEGAME_GAMECOMMAND_' + gcommands.pause, function(options) {
+        this.events.ng.on(CMD + gcommands.pause, function(options) {
             node.emit('BEFORE_GAMECOMMAND', gcommands.pause, options);
-            
+
             // TODO: check conditions
 
             node.game.pause();
@@ -165,7 +168,7 @@
          * ## NODEGAME_GAMECOMMAND: resume
          *
          */
-        this.events.ng.on('NODEGAME_GAMECOMMAND_' + gcommands.resume, function(options) {
+        this.events.ng.on(CMD + gcommands.resume, function(options) {
             node.emit('BEFORE_GAMECOMMAND', gcommands.resume, options);
 
             // TODO: check conditions
@@ -177,7 +180,7 @@
          * ## NODEGAME_GAMECOMMAND: resume
          *
          */
-        this.events.ng.on('NODEGAME_GAMECOMMAND_' + gcommands.step, function(options) {
+        this.events.ng.on(CMD + gcommands.step, function(options) {
             node.emit('BEFORE_GAMECOMMAND', gcommands.step, options);
 
             // TODO: check conditions
@@ -185,7 +188,7 @@
             node.game.step();
         });
 
-        this.incomingAdded = true;
+        this.internalAdded = true;
         this.silly('internal listeners added');
         return true;
     };
