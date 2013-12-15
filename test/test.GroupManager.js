b265b4596f6c3556b15d286abcb91756e77ac740
Stefano Balietti <futur.dorko@gmail.com>
2013-12-15 16:11:45 +0100

    added docs and test to GroupManager

--- test/test.GroupManager.js
+++ test/test.GroupManager.js
@@ -11,15 +11,37 @@ var J = ngc.JSUS;
 
 var gm = new GroupManager();
 
-gm.setElements(J.seq(1,10));
+gm.addElements(J.seq(1,10));
 
 var groupNames = ["A","B","C","D"];
+var invalidGroupNames = ["A","B","C","A"];
 
 var memberships;
 
 describe('GroupManager', function() {
-    
-    describe('#createGroups(["A","B","C","D"])', function() {
+
+   describe('#create(["A","B","C","A"])', function() {
+        it('should throw an error.', function() {
+            try {
+                gm.create(invalidGroupNames);
+                false.should.be.true;
+            }
+            catch(e) {
+                true.should.be.true;
+            }            
+        });
+    });
+ 
+    describe('#removeAll', function() {
+        before(function() {
+            gm.removeAll();
+        });
+        it('should remove all groups.', function() {
+            gm.groups.size().should.be.eql(0);
+        });
+    });
+
+    describe('#create(["A","B","C","D"])', function() {
         before(function() {
             gm.create(groupNames);
         });
@@ -29,15 +51,60 @@ describe('GroupManager', function() {
         });
     });
 
+
     describe('#match("RANDOM")', function() {
         it('should create a new random match the first time', function() {
             gm.match('RANDOM');
-            memberships = gm.getMemberships();
+            memberships = gm.getMemberships(true);
+            for (var i = 0; i < memberships.length; i++) {
+                (memberships[i].length >= 2).should.be.true;
+            }
         });
         it('should create a new random match every time', function() {
             gm.match('RANDOM');
             memberships = gm.getMemberships();
+            for (var i in memberships) {
+                if (memberships.hasOwnProperty(i)) {
+                    (memberships[i].length >= 2).should.be.true;
+                }
+            }
         });
     });
+
+    describe('#createNGroups()', function() {
+        before(function() {
+            gm.removeAll();
+            gm.createNGroups(4);
+        });
+        it('should create four groups.', function() {
+            var groupNamesCreated = gm.getGroupNames();
+            groupNames.length.should.be.eql(4);
+        });
+        it('should create four new groups, if done again.', function() {
+            var groupNamesCreated = gm.createNGroups(4);
+            groupNames.length.should.be.eql(4);
+        });
+     });
+
+    describe('#assign2Group()', function() {
+        it('should assign one element to a group.', function() {
+            var g = gm.assign2Group('Group1', 'A');
+            g.elements[0].should.be.eql('A');
+        });
+        it('should assign a set of elements to a group.', function() {
+            var g = gm.assign2Group('Group2', ['A','B']);
+            g.elements.should.be.eql(['A','B']);
+            g = gm.get('Group2').elements.should.be.eql(['A','B']);
+        });
+        it('should assign a set of players from a PlayerList.', function() {
+            var pl = new PlayerList(null, [
+                { id: 'A' }, { id: 'B' }
+            ]);
+            var g = gm.assign2Group('Group3', pl);
+            g.elements.should.be.eql(['A','B']);
+            g = gm.get('Group3').elements.should.be.eql(['A','B']);
+        });
+     });
+    
 
 });
