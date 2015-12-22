d5f9c7bf988a7025b0e58abbb99780e08f4f8936
Stefano Balietti <futur.dorko@gmail.com>
2015-12-22 19:20:48 +0100

    EventEmitter class tested

--- test/test.EventEmitter.js
+++ test/test.EventEmitter.js
@@ -34,6 +34,14 @@ describe('EventEmitter', function() {
         });
     });
 
+    describe('#setRecordChanges true', function() {
+        it('should activate recording changes', function() {
+            var out = ee.setRecordChanges(true);
+            out.should.eql(true);
+            ee.recordChanges.should.eql(true);
+        });
+    });
+
     describe('#on', function() {
         it('should add A event listener', function() {
             tmp.func1 =  function() { result.A = [1]; return 1; };
@@ -87,6 +95,21 @@ describe('EventEmitter', function() {
 
     });
 
+    describe('#getChanges', function() {
+        it('should return the changes', function() {
+            var out = ee.getChanges();
+            J.isArray(out.added).should.be.true;
+            J.isArray(out.removed).should.be.true;
+            out.added.length.should.eql(5);
+            out.removed.length.should.eql(0);
+            out.added[0].should.eql({type: 'A', listener: tmp.func1});
+            out.added[1].should.eql({type: 'A', listener: tmp.func2});
+            out.added[2].should.eql({type: 'B', listener: tmp.func3});
+            out.added[3].should.eql({type: 'B', listener: tmp.func4});
+            out.added[4].should.eql({type: 'B', listener: tmp.func5});
+        });
+    });
+
     describe('#size', function() {
         it('should return the number of events registered', function() {
             ee.size().should.eql(2); // A and B.
@@ -112,10 +135,26 @@ describe('EventEmitter', function() {
 
     describe('#once', function() {
         it('should add one event listener for C', function() {
-            tmp.funcC =  function() { result.C = 1; };
+            tmp.funcC =  function() { result.C = result.C ? result.C++ : 1 ; };
             ee.once('C', tmp.funcC);
 
             ee.events['C'].should.be.Function();
+        });
+    });
+
+    describe('#getChanges / 2', function() {
+        it('should return the changes', function() {
+            var out = ee.getChanges();
+            J.isArray(out.added).should.be.true;
+            J.isArray(out.removed).should.be.true;
+            out.added.length.should.eql(6);
+            out.removed.length.should.eql(0);
+            out.added[0].should.eql({type: 'A', listener: tmp.func1});
+            out.added[1].should.eql({type: 'A', listener: tmp.func2});
+            out.added[2].should.eql({type: 'B', listener: tmp.func3});
+            out.added[3].should.eql({type: 'B', listener: tmp.func4});
+            out.added[4].should.eql({type: 'B', listener: tmp.func5});
+            out.added[5].should.eql({type: 'C', listener: ee.events.C});
         });
     });
 
@@ -209,11 +248,7 @@ describe('EventEmitter', function() {
             result.B[2].should.be.eql(14);
         });
     });
-
-
     describe('#emit', function() {
-        before(function() {
-        });
         it('should return what the event listeners return', function() {
             var out = ee.emit('B', 1, 2, 3, 5);
             out.should.be.eql([2, (1+2+3+5)]);
@@ -224,6 +259,160 @@ describe('EventEmitter', function() {
                out.should.be.eql(1);
            });
     });
+
+    describe('#setRecordChanges false', function() {
+        it('should deactivate recording changes', function() {
+            var out = ee.setRecordChanges(false);
+            out.should.eql(false);
+            ee.recordChanges.should.eql(false);
+        });
+    });
+
+    describe('#emitAsync', function() {
+        it('should emit events asynchronously', function(done) {
+            var a;
+            ee.on('D', function(value) {
+                a.should.be.eql(1);
+                value.should.be.true;
+                done();
+            });
+            ee.emitAsync('D', true);
+            a = 1;
+        });
+    });
+
+    describe('#getChanges / 3', function() {
+        it('should return the changes (D not added)', function() {
+            var out = ee.getChanges();
+            J.isArray(out.added).should.be.true;
+            J.isArray(out.removed).should.be.true;
+            out.added.length.should.eql(6);
+            out.removed.length.should.eql(0);
+            out.added[0].should.eql({type: 'A', listener: tmp.func1});
+            out.added[1].should.eql({type: 'A', listener: tmp.func2});
+            out.added[2].should.eql({type: 'B', listener: tmp.func3});
+            out.added[3].should.eql({type: 'B', listener: tmp.func4});
+            out.added[4].should.eql({type: 'B', listener: tmp.func5});
+            out.added[5].should.eql({type: 'C', listener: ee.events.C});
+        });
+    });
+
+    describe('#setRecordChanges true', function() {
+        it('should activate recording changes', function() {
+            var out = ee.setRecordChanges(true);
+            out.should.eql(true);
+            ee.recordChanges.should.eql(true);
+        });
+    });
+
+    describe('#emit (once)', function() {
+        it('should fire the event only once', function() {
+            ee.emit('C');
+            ee.emit('C');
+            ee.emit('C');
+            result.C.should.eql(1);
+        });
+        it('should have removed the fired callback', function() {
+            ('undefined' === typeof ee.events.C).should.be.true;
+        });
+
+    });
+
+    describe('#getChanges / 4', function() {
+        it('should return the changes', function() {
+            var out = ee.getChanges();
+            J.isArray(out.added).should.be.true;
+            J.isArray(out.removed).should.be.true;
+            out.added.length.should.eql(6);
+            out.removed.length.should.eql(1);
+            out.added[0].should.eql({type: 'A', listener: tmp.func1});
+            out.added[1].should.eql({type: 'A', listener: tmp.func2});
+            out.added[2].should.eql({type: 'B', listener: tmp.func3});
+            out.added[3].should.eql({type: 'B', listener: tmp.func4});
+            out.added[4].should.eql({type: 'B', listener: tmp.func5});
+
+            out.added[5].type.should.eql('C');
+            out.added[5].listener.should.be.Function();
+
+            out.removed[0].should.eql({
+                type: 'C',
+                listener: out.added[5].listener
+            });
+        });
+    });
+
+    describe('#remove', function() {
+        it('should remove all listeners for event A', function() {
+            ee.remove('A');
+            ('undefined' === typeof ee.events.A).should.be.true;
+        });
+        it('should removed one listener for event B - function', function() {
+            tmp.removedB = ee.remove('B', tmp.func4);
+            ee.events.B.length.should.eql(2);
+            ee.events.B[0].should.not.eql(tmp.func4);
+            ee.events.B[1].should.not.eql(tmp.func4);
+        });
+        it('should return removed listener for event B - function', function() {
+            tmp.removedB[0].should.eql(tmp.func4);
+        });
+
+    });
+
+    describe('#getChanges clear / 5', function() {
+        it('should return the changes', function() {
+            var out = ee.getChanges(true);
+            J.isArray(out.added).should.be.true;
+            J.isArray(out.removed).should.be.true;
+            out.added.length.should.eql(6);
+            out.removed.length.should.eql(4);
+            out.added[0].should.eql({type: 'A', listener: tmp.func1});
+            out.added[1].should.eql({type: 'A', listener: tmp.func2});
+            out.added[2].should.eql({type: 'B', listener: tmp.func3});
+            out.added[3].should.eql({type: 'B', listener: tmp.func4});
+            out.added[4].should.eql({type: 'B', listener: tmp.func5});
+
+            out.added[5].type.should.eql('C');
+            out.added[5].listener.should.be.Function();
+
+            out.removed[0].should.eql({
+                type: 'C',
+                listener: out.added[5].listener
+            });
+
+            out.removed[1].should.eql({type: 'A', listener: tmp.func1});
+            out.removed[2].should.eql({type: 'A', listener: tmp.func2});
+            out.removed[3].should.eql({type: 'B', listener: tmp.func4});
+        });
+        it('should clear the changes object', function() {
+            ee.changes.added.length.should.eql(0);
+            ee.changes.removed.length.should.eql(0);
+        });
+    });
+
+    describe('#clear', function() {
+        it('should clear all events', function() {
+            ee.clear();
+            ee.events.should.eql({});
+        });
+    });
+
+    describe('#getChanges clear / 6', function() {
+        it('should return the changes', function() {
+            var out = ee.getChanges(true);
+            J.isArray(out.added).should.be.true;
+            J.isArray(out.removed).should.be.true;
+            out.added.length.should.eql(0);
+            out.removed.length.should.eql(3);
+
+            out.removed[0].should.eql({type: 'B', listener: tmp.func3});
+            out.removed[1].should.eql({type: 'B', listener: tmp.func5});
+
+            out.removed[2].type.should.eql('D');
+            out.removed[2].listener.should.be.Function();
+        });
+    });
+
+
 });
 
 
