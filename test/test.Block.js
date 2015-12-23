b0f8d6be3b213224642838651aea96cf8ffd7851
Stefano Balietti <futur.dorko@gmail.com>
2015-12-23 15:17:18 +0100

    minor changes in Block

--- test/test.Block.js
+++ test/test.Block.js
@@ -11,27 +11,50 @@ var block;
 var result;
 
 describe('#Block', function() {
-    before(function() {
 
-        block = new Block({
-            id: 'myblock',
-            type: 'mytype'
+    describe('#constructor', function() {
+
+        it('should create a new block with "linear" positions', function() {
+            block = new Block({
+                id: 'myblock',
+                type: 'mytype'
+            });
+            block.id.should.eql('myblock');
+            block.type.should.eql('mytype');
+            block.positions.should.eql('linear');
         });
 
-        // Type is: '__default', the name of the current block
-
-        // Step.
-        block.add({
-            id: 'item_A',
-            type: 'step',
-            item: { id: 'item_A' }
+        it('should fail if parameters are not correct', function() {
+            (function() {
+                block = new Block({
+                id: 'myblock',
+                type: ''
+                });
+            }).should.throw();
+            (function() {
+                block = new Block({
+                id: '',
+                type: 'aa'
+                });
+            }).should.throw();
+            (function() {
+                block = new Block({
+                id: 'myblock',
+                type: 1
+                });
+            }).should.throw();
+            (function() {
+                block = new Block({
+                id: null,
+                type: 'aa'
+                });
+            }).should.throw();
         });
-
-        // result = testPositions(stager, 100);
     });
 
-    it('should have removed default step from stage 1', function() {
-        // typeof(result['stage 1'] + '').should.eql('undefined');
+    describe('#add', function() {
+        it('should have removed default step from stage 1', function() {
+            // typeof(result['stage 1'] + '').should.eql('undefined');
+        });
     });
-
 });
