c5e0e882932a8e0ca5cbc7953fabf8e945efbd28
shakty <futur.dorko@gmail.com>
2021-01-26 13:52:52 +0100

    widgets can prevent scrollIntoView into view in widget steps

--- build/nodegame-full.js
+++ build/nodegame-full.js
@@ -24758,7 +24758,8 @@ if (!Array.prototype.indexOf) {
                          values.choice === null ||
                          values.isCorrect === false)) {
 
-                         if ('function' === typeof
+                         if (values._scrolledIntoView !== true &&
+                             'function' === typeof
                              widgetObj.bodyDiv.scrollIntoView) {
 
                              widgetObj.bodyDiv.scrollIntoView({
@@ -33689,8 +33690,8 @@ if (!Array.prototype.indexOf) {
      *
      * @emit INFOPANEL_GENERATED
      */
-    GameWindow.prototype.generateInfoPanel = function(root, options, force) {
-        var infoPanelDiv;
+    GameWindow.prototype.generateInfoPanel = function(options, force) {
+        var infoPanelDiv, root;
 
         if (this.infoPanel) {
             if (!force) {
@@ -34380,6 +34381,7 @@ if (!Array.prototype.indexOf) {
         var that;
         var loadCache;
         var storeCacheNow, storeCacheLater;
+        var scrollUp;
         var autoParse, autoParsePrefix, autoParseMod;
         var iframe, iframeName, iframeDocument, iframeWindow;
         var frameDocumentElement, frameReady;
@@ -34463,6 +34465,8 @@ if (!Array.prototype.indexOf) {
             }
         }
 
+        // Parsing options.
+
         if ('undefined' !== typeof opts.autoParse) {
             if ('object' !== typeof opts.autoParse) {
                 throw new TypeError('GameWindow.loadFrame: opts.autoParse ' +
@@ -34489,6 +34493,10 @@ if (!Array.prototype.indexOf) {
             }
             autoParse = opts.autoParse;
         }
+
+        // Scroll Up.
+
+        scrollUp = 'undefined' === typeof opts.scrollUp ? true : opts.scrollUp;
 
         // Store unprocessed uri parameter.
         this.unprocessedUri = uri;
@@ -34560,7 +34568,8 @@ if (!Array.prototype.indexOf) {
                                     that.updateLoadFrameState(func,
                                                               autoParse,
                                                               autoParseMod,
-                                                              autoParsePrefix);
+                                                              autoParsePrefix,
+                                                              scrollUp);
                                 });
             });
         }
@@ -34581,7 +34590,8 @@ if (!Array.prototype.indexOf) {
                                     that.updateLoadFrameState(func,
                                                               autoParse,
                                                               autoParseMod,
-                                                              autoParsePrefix);
+                                                              autoParsePrefix,
+                                                              scrollUp);
                                 });
             }
         }
@@ -34632,6 +34642,8 @@ if (!Array.prototype.indexOf) {
      * @param {string} autoParseMod Optional. Modifier for search and replace
      * @param {string} autoParsePrefix Optional. Custom prefix to add to the
      *    keys of the elements in autoParse object
+     * @param {boolean} scrollUp Optional. If TRUE, scrolls the page to the,
+     *    top (if window.scrollTo is defined). Default: FALSE.
      *
      * @see GameWindow.searchReplace
      * @see updateAreLoading
@@ -34641,7 +34653,8 @@ if (!Array.prototype.indexOf) {
      */
     GameWindow.prototype.updateLoadFrameState = function(func, autoParse,
                                                          autoParseMod,
-                                                         autoParsePrefix) {
+                                                         autoParsePrefix,
+                                                         scrollUp) {
 
         var loaded, stageLevel;
         loaded = updateAreLoading(this, -1);
@@ -34650,6 +34663,7 @@ if (!Array.prototype.indexOf) {
         if (autoParse) {
             this.searchReplace(autoParse, autoParseMod, autoParsePrefix);
         }
+        if (scrollUp && window.scrollTo) window.scrollTo(0,0);
 
         // ng event emitter is not used.
         node.events.ee.game.emit('FRAME_LOADED');
@@ -40365,7 +40379,7 @@ if (!Array.prototype.indexOf) {
             unhighlighted: []
         };
 
-        // By default destoy widget on exit step.
+        // By default destroy widget on exit step.
         widget.destroyOnExit = options.destroyOnExit !== false;
 
         // Required widgets require action from user, otherwise they will
@@ -44453,7 +44467,8 @@ if (!Array.prototype.indexOf) {
             }
             else {
                 obj.forms[form.id] = form.getValues(opts);
-                if (form.requiredChoice &&
+                // Backward compatible (requiredChoice).
+                if ((form.required || form.requiredChoice) &&
                     (obj.forms[form.id].choice === null ||
                      (form.selectMultiple &&
                       !obj.forms[form.id].choice.length))) {
@@ -52612,7 +52627,7 @@ if (!Array.prototype.indexOf) {
 
 /**
  * # EndScreen
- * Copyright(c) 2020 Stefano Balietti <ste@nodegame.org>
+ * Copyright(c) 2021 Stefano Balietti <ste@nodegame.org>
  * MIT Licensed
  *
  * Creates an interface to display final earnings, exit code, etc.
@@ -52628,7 +52643,7 @@ if (!Array.prototype.indexOf) {
 
     // ## Add Meta-data
 
-    EndScreen.version = '0.7.0';
+    EndScreen.version = '0.7.1';
     EndScreen.description = 'Game end screen. With end game message, ' +
                             'email form, and exit code.';
 
@@ -52676,21 +52691,7 @@ if (!Array.prototype.indexOf) {
          *
          * Default: true
          */
-        if (options.email === false) {
-            options.showEmailForm = false;
-        }
-        else if ('undefined' === typeof options.showEmailForm) {
-            this.showEmailForm = true;
-        }
-        else if ('boolean' === typeof options.showEmailForm) {
-            this.showEmailForm = options.showEmailForm;
-        }
-        else {
-            throw new TypeError('EndScreen constructor: ' +
-                                'options.showEmailForm ' +
-                                'must be boolean or undefined. ' +
-                                'Found: ' + options.showEmailForm);
-        }
+        this.showEmailForm = true;
 
         /**
          * ### EndScreen.showFeedbackForm
@@ -52699,21 +52700,7 @@ if (!Array.prototype.indexOf) {
          *
          * Default: true
          */
-        if (options.feedback === false) {
-            options.showFeedbackForm = false;
-        }
-        else if ('undefined' === typeof options.showFeedbackForm) {
-            this.showFeedbackForm = true;
-        }
-        else if ('boolean' === typeof options.showFeedbackForm) {
-            this.showFeedbackForm = options.showFeedbackForm;
-        }
-        else {
-            throw new TypeError('EndScreen constructor: ' +
-                                'options.showFeedbackForm ' +
-                                'must be boolean or undefined. ' +
-                                'Found: ' + options.showFeedbackForm);
-        }
+        this.showFeedbackForm = true;
 
         /**
          * ### EndScreen.showTotalWin
@@ -52722,21 +52709,7 @@ if (!Array.prototype.indexOf) {
          *
          * Default: true
          */
-        if (options.totalWin === false) {
-            options.showTotalWin = false;
-        }
-        else if ('undefined' === typeof options.showTotalWin) {
-            this.showTotalWin = true;
-        }
-        else if ('boolean' === typeof options.showTotalWin) {
-            this.showTotalWin = options.showTotalWin;
-        }
-        else {
-            throw new TypeError('EndScreen constructor: ' +
-                                'options.showTotalWin ' +
-                                'must be boolean or undefined. ' +
-                                'Found: ' + options.showTotalWin);
-        }
+         this.showTotalWin = true;
 
         /**
          * ### EndScreen.showExitCode
@@ -52745,21 +52718,7 @@ if (!Array.prototype.indexOf) {
          *
          * Default: true
          */
-        if (options.exitCode === false) {
-            options.showExitCode !== false
-        }
-        else if ('undefined' === typeof options.showExitCode) {
-            this.showExitCode = true;
-        }
-        else if ('boolean' === typeof options.showExitCode) {
-            this.showExitCode = options.showExitCode;
-        }
-        else {
-            throw new TypeError('EndScreen constructor: ' +
-                                'options.showExitCode ' +
-                                'must be boolean or undefined. ' +
-                                'Found: ' + options.showExitCode);
-        }
+        this.showExitCode = true;
 
         /**
          * ### EndScreen.totalWinCurrency
@@ -52768,20 +52727,7 @@ if (!Array.prototype.indexOf) {
          *
          * Default: 'USD'
          */
-        if ('undefined' === typeof options.totalWinCurrency) {
-            this.totalWinCurrency = 'USD';
-        }
-        else if ('string' === typeof options.totalWinCurrency &&
-                 options.totalWinCurrency.trim() !== '') {
-
-            this.totalWinCurrency = options.totalWinCurrency;
-        }
-        else {
-            throw new TypeError('EndScreen constructor: ' +
-                                'options.totalWinCurrency must be undefined ' +
-                                'or a non-empty string. Found: ' +
-                                options.totalWinCurrency);
-        }
+         this.totalWinCurrency = 'USD';
 
         /**
          * ### EndScreen.totalWinCb
@@ -52791,17 +52737,7 @@ if (!Array.prototype.indexOf) {
          * Accepts two parameters: a data object (as sent from server), and
          * the reference to the EndScreen.
          */
-        if (options.totalWinCb) {
-            if ('function' === typeof options.totalWinCb) {
-                this.totalWinCb = options.totalWinCb;
-            }
-            else {
-                throw new TypeError('EndScreen constructor: ' +
-                                    'options.totalWinCb ' +
-                                    'must be function or undefined. ' +
-                                    'Found: ' + options.totalWinCb);
-            }
-        }
+        this.totalWinCb = null;
 
         /**
          * ### EndScreen.emailForm
@@ -52843,7 +52779,81 @@ if (!Array.prototype.indexOf) {
 
     EndScreen.prototype.init = function(options) {
 
+        if (options.email === false) {
+            this.showEmailForm = false;
+        }
+        else if ('boolean' === typeof options.showEmailForm) {
+            this.showEmailForm = options.showEmailForm;
+        }
+        else if ('undefined' !== typeof options.showEmailForm) {
+            throw new TypeError('EndScreen.init: ' +
+                                'options.showEmailForm ' +
+                                'must be boolean or undefined. ' +
+                                'Found: ' + options.showEmailForm);
+        }
 
+        if (options.feedback === false) {
+            this.showFeedbackForm = false;
+        }
+        else if ('boolean' === typeof options.showFeedbackForm) {
+            this.showFeedbackForm = options.showFeedbackForm;
+        }
+        else if ('undefined' !== typeof options.showFeedbackForm) {
+            throw new TypeError('EndScreen.init: ' +
+                                'options.showFeedbackForm ' +
+                                'must be boolean or undefined. ' +
+                                'Found: ' + options.showFeedbackForm);
+        }
+
+        if (options.totalWin === false) {
+            this.showTotalWin = false;
+        }
+        else if ('boolean' === typeof options.showTotalWin) {
+            this.showTotalWin = options.showTotalWin;
+        }
+        else if ('undefined' !== typeof options.showTotalWin) {
+            throw new TypeError('EndScreen.init: ' +
+                                'options.showTotalWin ' +
+                                'must be boolean or undefined. ' +
+                                'Found: ' + options.showTotalWin);
+        }
+
+        if (options.exitCode === false) {
+            options.showExitCode !== false
+        }
+        else if ('boolean' === typeof options.showExitCode) {
+            this.showExitCode = options.showExitCode;
+        }
+        else if ('undefined' !== typeof options.showExitCode) {
+            throw new TypeError('EndScreen.init: ' +
+                                'options.showExitCode ' +
+                                'must be boolean or undefined. ' +
+                                'Found: ' + options.showExitCode);
+        }
+
+        if ('string' === typeof options.totalWinCurrency &&
+                 options.totalWinCurrency.trim() !== '') {
+
+            this.totalWinCurrency = options.totalWinCurrency;
+        }
+        else if ('undefined' !== typeof options.totalWinCurrency) {
+            throw new TypeError('EndScreen.init: ' +
+                                'options.totalWinCurrency must be undefined ' +
+                                'or a non-empty string. Found: ' +
+                                options.totalWinCurrency);
+        }
+
+        if (options.totalWinCb) {
+            if ('function' === typeof options.totalWinCb) {
+                this.totalWinCb = options.totalWinCb;
+            }
+            else {
+                throw new TypeError('EndScreen.init: ' +
+                                    'options.totalWinCb ' +
+                                    'must be function or undefined. ' +
+                                    'Found: ' + options.totalWinCb);
+            }
+        }
 
         if (this.showEmailForm && !this.emailForm) {
             // TODO: nested properties are overwitten fully. Update.
@@ -56227,13 +56237,17 @@ if (!Array.prototype.indexOf) {
                     var cl;
                     // Set global variables.
                     // slider.getValues().value fails (no int numbers).
-                    finalValue = parseInt(slider.slider.value, 10),
-                    isWinner = finalValue < bombBox;
+                    finalValue = parseInt(slider.slider.value, 10);
+
                     // Update table.
                     if (bombBox > -1) {
                         // Variable bombBox is between 1 and totBoxes.
                         // Cells in table are 0-indexed.
                         W.gid(getBoxId(bombBox-1)).style.background = '#fa0404';
+                        isWinner = finalValue < bombBox;
+                    }
+                    else {
+                        isWinner = true;
                     }
                     // Hide slider and button
                     slider.hide();
